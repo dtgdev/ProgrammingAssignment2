f0002cd09c8d133259e8a33fdfaa22657908dcdb
Dawit Gebremichael <dtgdev@yahoo.com>
2014-06-22 20:53:50 -0500

    compute inverse Matrix and provide set and get Matrix
    compute inverse Matrix and provide set and get Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 
+## This function set and get the value of metrix and inverse metrix and
+## return list
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  #initialize the varaible that holds metrix to null
+  inverseMatrix <- NULL
+  #Set and Get method to assign value to inverseMatrix variable
+  setinverseMatrix <- function(value) inverseMatrix <<- value
+  getinverseMatrix <- function() inverseMatrix
+  
+  set <- function(y) {
+    x <<- y
+    inverseMatrix <<- NULL
+  }
+  
+  get <- function() x
+  
+  list( setinverseMatrix = setinverseMatrix, getinverseMatrix = getinverseMatrix, set = set, get = get)
 }
 
 
-## Write a short comment describing this function
-
+## This function returns the invers of a given x
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinv()
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setinverse(inverse)
+  
+  inverse
 }
