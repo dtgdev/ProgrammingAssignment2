027d226203ead8d53d177002ba10c787f84f840e
Dawit Gebremichael <dtgdev@yahoo.com>
2014-08-11 00:05:37 -0500

    t

--- plot3.R
+++ plot3.R
@@ -1,6 +1,20 @@
-@gray-darker:  lighten(#000, 13.5%); // #222
-@gray-dark:    lighten(#000, 20%);   // #333
-@gray:         lighten(#000, 33.5%); // #555
-@gray-light:   lighten(#000, 46.7%); // #777
-@gray-lighter: lighten(#000, 93.5%); // #eee
-benign appearing findings we encourage y
+
+#read 2007-02-01 and 2007-02-02 through cat pipline and redirecting the output to data.txt
+#head -n 1 ./data/household_power_consumption.txt > data.txt
+#cat household_power_consumption.txt  | grep '^0\{0,1\}[12]/0\{0,1\}2/2007' >> data.txt
+
+#read power consumption
+data <- read.table("data.txt", sep = ";", header = T)
+time <- paste(data$Date, data$Time)
+data$DateTime <- strptime(time, "%d/%m/%Y %H:%M:%S")
+head(data, 3)
+
+with(data, plot(DateTime,Sub_metering_1, ylab = "Energy sub metering", type = "l")) ## Create plot on screen device
+with(data,points(DateTime,Sub_metering_2, type = "l", xlab = "", ylab = "Energy sub metering", col = "red"))
+with(data,points(DateTime,Sub_metering_3, type = "l", xlab = "", ylab = "Energy sub metering", col = "blue"))
+legend("topright",col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lty = 1)
+
+
+
+dev.copy(png, file = "plot3.png") ## Copy my plot to a PNG file
+dev.off() ## Don't forget to close the PNG device!
