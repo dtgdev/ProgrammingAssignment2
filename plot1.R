a51b7518e1b1282225cca313ece5c889c73db567
Dawit Gebremichael <dtgdev@yahoo.com>
2014-08-10 23:02:55 -0500

    Plot1 - plot1.png

--- plot1.R
+++ plot1.R
@@ -1,6 +1,10 @@
-@gray-darker:  lighten(#000, 13.5%); // #222
-@gray-dark:    lighten(#000, 20%);   // #333
-@gray:         lighten(#000, 33.5%); // #555
-@gray-light:   lighten(#000, 46.7%); // #777
-@gray-lighter: lighten(#000, 93.5%); // #eee
-benign appearing findings we encourage y
+
+#read 2007-02-01 and 2007-02-02 through cat pipline and redirecting the output to data.txt
+head -n 1 ./data/household_power_consumption.txt > data.txt
+cat household_power_consumption.txt  | grep '^0\{0,1\}[12]/0\{0,1\}2/2007' >> data.txt
+
+#read power consumption
+data <- read.table("data.txt", sep = ";", header = T)
+
+hist(data$Global_active_power,col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)" )
+
