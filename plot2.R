fcf3fbc9ca686f7b31786739094809dc30229c06
Dawit Gebremichael <dtgdev@yahoo.com>
2014-08-10 23:14:13 -0500

    t

--- plot2.R
+++ plot2.R
@@ -1,6 +1,15 @@
-@gray-darker:  lighten(#000, 13.5%); // #222
-@gray-dark:    lighten(#000, 20%);   // #333
-@gray:         lighten(#000, 33.5%); // #555
-@gray-light:   lighten(#000, 46.7%); // #777
-@gray-lighter: lighten(#000, 93.5%); // #eee
-benign appearing findings we encourage y
+
+#read 2007-02-01 and 2007-02-02 through cat pipline and redirecting the output to data.txt
+head -n 1 ./data/household_power_consumption.txt > data.txt
+cat household_power_consumption.txt  | grep '^0\{0,1\}[12]/0\{0,1\}2/2007' >> data.txt
+
+#read power consumption
+data <- read.table("data.txt", sep = ";", header = T)
+
+with(data, plot(Global_active_power, ylab = "Global Active Power (kilowatts)",type = "l")) ## Create plot on screen device
+title(main = "Global Active Power") ## Add a main title
+
+
+
+dev.copy(png, file = "plot2.png") ## Copy my plot to a PNG file
+dev.off() ## Don't forget to close the PNG device!
